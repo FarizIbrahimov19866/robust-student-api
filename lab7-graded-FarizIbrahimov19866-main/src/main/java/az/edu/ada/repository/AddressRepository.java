4399d0fed61b11eaef63fe6091c6ff8381c619cb
FarizIbrahimov19866 <151494700+FarizIbrahimov19866@users.noreply.github.com>
2026-05-11 21:06:33 +0400

    Delete lab7-graded-FarizIbrahimov19866-main directory

--- lab7-graded-FarizIbrahimov19866-main/src/main/java/az/edu/ada/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-package az.edu.ada.repository;
-
-import az.edu.ada.model.Address;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepository extends JpaRepository<Address, Long> {
-}
--- lab7-graded-FarizIbrahimov19866-main/src/main/java/az/edu/ada/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package az.edu.ada.repository;
-
-import az.edu.ada.model.Student;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StudentRepository extends JpaRepository<Student, Long> {
-}
